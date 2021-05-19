1bb497509c1d74aef5dbd449a9baeeca9e9268fb
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2021-05-19 21:12:47 +0200

    Add styles

--- Front/app.js
+++ Front/app.js
@@ -1,32 +1,19 @@
-console.log("Vue")
 
-var app = new Vue({
-    el: '#Message',
-    data :{
-        message:'Hello Vue!'
-    }
-})
 
-var app = new Vue({
-    el:`#Reverse`,
-    data: {
-        message: 'Hello Vue.js!'
-    },
-    methods: {
-        reverseMessage: function(){
-            this.message = this.message.split('').reverse().join('');
+const app = Vue.createApp({
+    data (){
+        return {
+            message: 'Message Injection',
         }
     }
-})
 
-var app = new Vue({
-    el: '#Request',
-    data:{
-        message:'Get google.com'
-    },
-    methods:{
-        getRequest: function(){
-            this.message = this.$http.get('google.com');
-        }
-    }
 })
+// const app1 = Vue.createApp()
+// const app2 = Vue.createApp()
+
+app.mount('#Message')
+// app1.mount('#Reverse')
+// app2.mount('#Request')
+
+
+
