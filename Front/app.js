1925d44a5044fb17985659f44c44287218d5a3c2
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2021-05-20 00:31:30 +0200

    Add some basic elements to page

--- Front/app.js
+++ Front/app.js
@@ -4,14 +4,29 @@ const app = Vue.createApp({
     data (){
         return {
             message: 'Message Injection',
+            message2: 'Message in other div'
+        }
+    }
+    
+
+})
+
+const app2 = Vue.createApp({
+    data (){
+        return {
+            message2: 'Message in other div'
+        }
+    },
+    methods: {
+        reverseMessage() {
+            this.message2 = this.message2.split('').reverse().join('');
         }
     }
 
 })
-// const app1 = Vue.createApp()
-// const app2 = Vue.createApp()
 
 app.mount('#Message')
+app2.mount('#Reverse')
 // app1.mount('#Reverse')
 // app2.mount('#Request')
 
