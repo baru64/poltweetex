f02f1cbfbd80d567a20089f6d241e6f87a942320
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2021-05-23 21:18:00 +0200

    CSS

--- Front/app.js
+++ Front/app.js
@@ -6,6 +6,15 @@ const app = Vue.createApp({
             sejm: 'Sejm',
             polityk: 'Polityk',
             posel: 'Poseł',
+            tweets: [
+                {nr: 1, name: 'Filip', surname: 'Piwowarczyk', party: 'Husaria', word: 'Kurde', lastTT:'20:10 2021.03.04', count: 3},
+                {nr: 2, name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
+                {nr: 3, name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
+                {nr: 4, name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
+                {nr: 5, name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
+                {nr: 6, name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
+                {nr: 2, name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
+            ]
         }
         
     },
@@ -22,22 +31,5 @@ const app = Vue.createApp({
 
 })
 
-const app2 = Vue.createApp({
-    data (){
-        return {
-            tweets: [
-                {nr: 1, name: 'Filip', surname: 'Piwowarczyk', party: 'Husaria', word: 'Kurde', lastTT:'20:10 2021.03.04', count: 3},
-                {nr: 2, name: 'Filip', surname: 'Piwowarczyk', party: 'Husaria', word: 'Kurde', lastTT:'20:10 2021.03.04', count: 5}
-            ]
-        }
-    },
-    methods: {
-        reverseMessage() {
-            this.message2 = this.message2.split('').reverse().join('');
-        }
-    }
-
-})
-app.mount("#Search")
-app2.mount('#Table')
+app.mount("#app")
 
