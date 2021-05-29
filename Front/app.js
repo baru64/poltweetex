b84e87c686db5981c9329c6f1629df7f1c6c090f
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2021-05-29 15:42:36 +0200

    Add website flow

--- Front/app.js
+++ Front/app.js
@@ -1,22 +1,33 @@
 const app = Vue.createApp({
     data(){
-        return{
+        return {
             word: '',
-            title: 'Title',
-            sejm: 'Sejm',
-            polityk: 'Polityk',
-            posel: 'Poseł',
+            title: 'Twitter Of Politics',
+            showParties: false,
+            showPoliticsFromParty: false,
+            showPoliticsFromSejm: false,
+            party: '',
             tweets: [
-                {nr: 1, name: 'Filip', surname: 'Piwowarczyk', party: 'Husaria', word: 'Kurde', lastTT:'20:10 2021.03.04', count: 3},
-                {nr: 2, name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
-                {nr: 3, name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
-                {nr: 4, name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
-                {nr: 5, name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
-                {nr: 6, name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
-                {nr: 2, name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
+                {name: 'Filip', surname: 'Piwowarczyk', party: 'Husaria', word: 'Kurde', lastTT:'20:10 2021.03.04', count: 3},
+                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
+                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Elo', lastTT:'20:10 2021.03.04', count: 999},
+                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Yo', lastTT:'20:10 2021.03.04', count: 123},
+                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Mordo', lastTT:'20:10 2021.03.04', count:-12},
+                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Co', lastTT:'20:10 2021.03.04', count: 0},
+                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Tu', lastTT:'20:10 2021.03.04', count: -123},
+                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Robisz', lastTT:'20:10 2021.03.04', count: 1.5},
+            ],
+            politics: [
+                {name: 'Jan', surname: 'Brzechwa', id:12343321},
+                {name: 'Karol', surname: 'Krawczyk', id:12343321},
+                {name: 'Twoja', surname: 'Mama', id:12343321},
+            ],
+            parties: [
+                {name: 'PO'},
+                {name: 'PSL'},
+                {name: 'LEWICA'},
             ]
         }
-        
     },
     methods: {
         sumitSearch(word){
@@ -25,11 +36,43 @@ const app = Vue.createApp({
         },
         changeTitle(buttonName){
             this.title = buttonName;
+
+        },
+        getSejm(){
+            console.log("Get All words in Sejm");
+            resetState(this);
+        },
+        getParty(){
+            console.log("Get Words in Party and show Politics of Party");
+            this.showParties = !this.showParties;
+            this.showPoliticsFromSejm = false;
+        },
+        getPoselsFromParty(party){
+            console.log("Get Posels From");
+            this.showPoliticsFromSejm = false;
+            this.showPoliticsFromParty = true;
+            this.party = party
+        },
+        getPoselsFromSejm(){
+            console.log("Get Posels From Sejm");
+            this.party = 'Sejm';
+            this.showParties = false;
+            this.showPoliticsFromParty = false;
+            this.showPoliticsFromSejm = true;
+        },
+        getPoliticainWords(){
+            console.log("Output Politican Words");
         }
     }
-    
+});
 
-})
 
+function resetState(ctx){
+    ctx.word = '';
+    ctx.party = '';
+    ctx.showParties = false;
+    ctx.showPoliticsFromParty = false;
+    ctx.showPoliticsFromSejm = false;
+}
 app.mount("#app")
 
