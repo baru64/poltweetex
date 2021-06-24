e44690345c4d6705ef4ce73bef5b6526f8dc39c4
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2021-06-24 23:11:11 +0200

    i dont know

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,30 @@
-import os
+
+# import os
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:{}@{}/{}".format(
+#     os.environ['POSTGRESQL_PASSWORD'],
+#     os.environ['POSTGRESQL_URL'],
+#     os.environ['POSTGRESQL_NAME']
+# )
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:{}@{}/{}".format(
-    os.environ['POSTGRESQL_PASSWORD'],
-    os.environ['POSTGRESQL_URL'],
-    os.environ['POSTGRESQL_NAME']
+    'docker',
+    'localhost:5432',
+    'postgres'
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- worker/database.py
+++ worker/database.py
@@ -1,4 +1,21 @@
 
+# import os
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:{}@{}/{}".format(
+#     os.environ['POSTGRESQL_PASSWORD'],
+#     os.environ['POSTGRESQL_URL'],
+#     os.environ['POSTGRESQL_NAME']
+# )
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
 import os
 
 from sqlalchemy import create_engine
@@ -7,8 +24,8 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:{}@{}/{}".format(
     os.environ['POSTGRESQL_PASSWORD'],
-    os.environ['POSTGRESQL_URL'],
-    os.environ['POSTGRESQL_NAME']
+    'postgres-postgresql',
+    'exampledb'
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
