cc9d89860c765cd349c9ab3d5ffee11d559aca2c
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2021-06-24 17:15:31 +0200

    Format code

--- worker/database.py
+++ worker/database.py
@@ -1,3 +1,21 @@
+
+# import os
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:{}@{}/{}".format(
+#     os.environ['POSTGRESQL_PASSWORD'],
+#     os.environ['POSTGRESQL_URL'],
+#     os.environ['POSTGRESQL_NAME']
+# )
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
 import os
 
 from sqlalchemy import create_engine
@@ -5,9 +23,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:{}@{}/{}".format(
-    os.environ['POSTGRESQL_PASSWORD'],
-    'postgres-postgresql',
-    'exampledb'
+    'docker',
+    'localhost:5432',
+    'postgres'
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
---
+++ worker/schemas.py
@@ -0,0 +1,72 @@
+from pydantic import BaseModel
+
+# Party schema
+
+
+class PartyBase(BaseModel):
+    name: str
+
+
+class PartyCreate(PartyBase):
+    pass
+
+
+class Party(PartyBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+# Politicians schema
+
+
+class PoliticianBase(BaseModel):
+    name: str
+    twitter_id: str
+    party_id: int
+
+
+class PoliticianCreate(PoliticianBase):
+    pass
+
+
+class Politician(PoliticianBase):
+    class Config:
+        orm_mode = True
+
+# Word schema
+
+
+class WordBase(BaseModel):
+    word: str
+    politician_id: int
+    tweet_id: str
+    count: int
+
+
+class WordCreate(WordBase):
+    pass
+
+
+class Word(WordBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+# WrodIndex schema
+
+
+class WordIndexBase(BaseModel):
+    word: str
+
+
+class WordIndexCreate(WordIndexBase):
+    pass
+
+
+class WordIndex(WordIndexBase):
+    id: int
+
+    class Config:
+        orm_mode = True
