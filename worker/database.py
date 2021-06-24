9040482bec162e1f1a8b8a285457656d79892f91
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2021-06-24 17:17:38 +0200

    Fix db

--- worker/database.py
+++ worker/database.py
@@ -1,21 +1,4 @@
 
-# import os
-
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-
-# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:{}@{}/{}".format(
-#     os.environ['POSTGRESQL_PASSWORD'],
-#     os.environ['POSTGRESQL_URL'],
-#     os.environ['POSTGRESQL_NAME']
-# )
-
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base = declarative_base()
 import os
 
 from sqlalchemy import create_engine
@@ -23,9 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:{}@{}/{}".format(
-    'docker',
-    'localhost:5432',
-    'postgres'
+    os.environ['POSTGRESQL_PASSWORD'],
+    os.environ['POSTGRESQL_URL'],
+    os.environ['POSTGRESQL_NAME']
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
