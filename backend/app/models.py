be369f39f2212fed7a4dc2ecb523c681da18e213
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2021-06-24 22:13:21 +0200

    XD

--- backend/app/models.py
+++ backend/app/models.py
@@ -27,7 +27,7 @@ class Word(Base):
     politician_id = Column(String, index=True)
     tweet_id = Column(String, index=True)
     count = Column(Integer, index=True)
-    date = Column(DateTime, intex=True)
+    date = Column(DateTime, index=True)
 
 
 class WordIndex(Base):
