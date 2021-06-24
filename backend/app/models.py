4cee50a6de0f76d6026aed5f71266a29c04368f6
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2021-06-24 14:03:50 +0200

    Add modes and change to local db only for these branch

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,3 +9,34 @@ class Item(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
+
+class Party(Base):
+    __tablename__ = "parties"
+
+    id = Column(Integer, primary_key=True, index= True)
+    name = Column(String, index= True)
+
+class Politician(Base):
+    __tablename__ = "politicians"
+
+    id = Column(Integer, primary_key=True, index=True)
+    twitter_ID = Column(String, index=True)
+    party_ID = Column(Integer,index=True)
+
+
+class Word(Base):
+    __tablename__="words"
+
+    id = Column(Integer, primary_key=True, index=True)
+    word = Column(String, index=True)
+    politician_id = Column(Integer, index=True)
+    tweet_id = Column(String, index=True)
+    count = Column(Integer,index=True)
+
+
+class WordIndex(Base):
+    __tablename__ = "wordIndexes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    word = Column(String, index=True)
+
