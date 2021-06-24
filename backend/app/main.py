1e3bf327c29c684fd37e7c59e45a0fc57c9225f7
Bartosz Krakowiak <bartosz_krakowiak@protonmail.com>
2021-06-24 17:20:20 +0200

    comment out startup

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,27 +12,27 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-@app.on_event("startup")
-async def startup_event():
-    with open("./app/data/parties.json",'r') as politicians_json:
-        parties = json.load(politicians_json)
-    with SessionLocal() as db:
-        crud.delete_all_parties(db)
-        crud.delete_all_politicians(db)
-        for party in parties:
-            new_party = models.Party(
-                id = party["party_ID"],
-                name = party['Party']
-            )
-            for politician in party["Politicians"]:
-                new_politician = models.Politician(
-                    twitter_id=politician['ID'],
-                    name = politician['Name'],
-                    party_id=party["party_ID"]
-                )
-                db.add(new_politician)
-            db.add(new_party)
-        db.commit()
+# @app.on_event("startup")
+# async def startup_event():
+#     with open("./app/data/parties.json",'r') as politicians_json:
+#         parties = json.load(politicians_json)
+#     with SessionLocal() as db:
+#         crud.delete_all_parties(db)
+#         crud.delete_all_politicians(db)
+#         for party in parties:
+#             new_party = models.Party(
+#                 id = party["party_ID"],
+#                 name = party['Party']
+#             )
+#             for politician in party["Politicians"]:
+#                 new_politician = models.Politician(
+#                     twitter_id=politician['ID'],
+#                     name = politician['Name'],
+#                     party_id=party["party_ID"]
+#                 )
+#                 db.add(new_politician)
+#             db.add(new_party)
+#         db.commit()
 
 # dependency
 def get_db():
