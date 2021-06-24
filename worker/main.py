9430b8d421b4836cc8d24341c5f96597c0af3e7a
Bartosz Krakowiak <bartosz_krakowiak@protonmail.com>
2021-06-24 14:29:13 +0200

    use non-indexed jobs

--- worker/main.py
+++ worker/main.py
@@ -1,18 +1,18 @@
-import os
+# import os
 from datetime import datetime, timezone
 
 from . import models
 from .database import SessionLocal
 
-INDEX = int(os.environ['JOB_COMPLETION_INDEX'])
+# INDEX = int(os.environ['JOB_COMPLETION_INDEX'])
 
 
 def main():
     with SessionLocal() as db:
         items = db.query(models.Item).all()
-        item = items[INDEX]
-        item.description = datetime.now(tz=timezone.utc).isoformat()
-        db.add(item)
+        for item in items:
+            item.description = datetime.now(tz=timezone.utc).isoformat()
+            db.add(item)
         db.commit()
 
 
