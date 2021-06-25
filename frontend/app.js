b6a073878c03f48a9ac873e194ac055053b75819
fpiwowarczyk <filip.piwowarczyk1997@gmail.com>
2021-06-25 11:50:29 +0200

    Add fetching parties and politicians from db

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 const app = Vue.createApp({
-    data(){
+    data() {
         return {
             word: '',
             title: 'Twitter Of Politics',
@@ -8,64 +8,63 @@ const app = Vue.createApp({
             showPoliticsFromSejm: false,
             party: '',
             tweets: [
-                {name: 'Filip', surname: 'Piwowarczyk', party: 'Husaria', word: 'Kurde', lastTT:'20:10 2021.03.04', count: 3},
-                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT:'20:10 2021.03.04', count: 5},
-                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Elo', lastTT:'20:10 2021.03.04', count: 999},
-                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Yo', lastTT:'20:10 2021.03.04', count: 123},
-                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Mordo', lastTT:'20:10 2021.03.04', count:-12},
-                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Co', lastTT:'20:10 2021.03.04', count: 0},
-                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Tu', lastTT:'20:10 2021.03.04', count: -123},
-                {name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Robisz', lastTT:'20:10 2021.03.04', count: 1.5},
+                { name: 'Filip', surname: 'Piwowarczyk', party: 'Husaria', word: 'Kurde', lastTT: '20:10 2021.03.04', count: 3 },
+                { name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'SIemanko', lastTT: '20:10 2021.03.04', count: 5 },
+                { name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Elo', lastTT: '20:10 2021.03.04', count: 999 },
+                { name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Yo', lastTT: '20:10 2021.03.04', count: 123 },
+                { name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Mordo', lastTT: '20:10 2021.03.04', count: -12 },
+                { name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Co', lastTT: '20:10 2021.03.04', count: 0 },
+                { name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Tu', lastTT: '20:10 2021.03.04', count: -123 },
+                { name: 'Filip', surname: 'Piwowarczyk', party: 'PO', word: 'Robisz', lastTT: '20:10 2021.03.04', count: 1.5 },
             ],
             politics: [
-                {name: 'Jan', surname: 'Brzechwa', id:12343321},
-                {name: 'Karol', surname: 'Krawczyk', id:12343321},
-                {name: 'Twoja', surname: 'Mama', id:12343321},
+                { name: 'Jan', surname: 'Brzechwa', id: 12343321 },
+                { name: 'Karol', surname: 'Krawczyk', id: 12343321 },
+                { name: 'Twoja', surname: 'Mama', id: 12343321 },
             ],
-            parties: [
-                {name: 'PO'},
-                {name: 'PSL'},
-                {name: 'LEWICA'},
-            ]
+            parties: []
         }
     },
     methods: {
-        sumitSearch(word){
+        sumitSearch(word) {
             console.log(word);
             this.word = '';
         },
-        getSejm(){
+        getSejm() {
             console.log("Get All words from DB");
             resetState(this);
         },
-        getParty(){
-            console.log("Get Parties from DB");
-            console.log("Get Words for Party from DB");
+        async getParty() {
+            const response = await axios.get('https://poltweetex.northeurope.cloudapp.azure.com/parties')
+            this.parties = response.data
             this.showPoliticsFromSejm = false;
             this.showParties = !this.showParties;
         },
-        getPoselsFromParty(party){
+        getPoselsFromParty(party) {
             console.log("Get Posels in Party from DB");
             this.showPoliticsFromSejm = false;
             this.party = party
             this.showPoliticsFromParty = true;
         },
-        getPoselsFromSejm(){
-            console.log("Get Posels in Sejm from DB");
-            this.party = 'Sejm';
+        async getPoselsFromSejm() {
+            const response = await axios.get('https://poltweetex.northeurope.cloudapp.azure.com/politicians')
+            console.log(response);
+            this.politics = response.data
+            this.party = 'Sejmu';
             this.showParties = false;
             this.showPoliticsFromParty = false;
             this.showPoliticsFromSejm = true;
+
         },
-        getPoliticainWords(){
+        getPoliticainWords() {
             fetch("http://127.0.0.1:8000/rndtweets")
-            .then(response => response.json)
+                .then(response => response.json)
         }
     }
 });
 
 
-function resetState(ctx){
+function resetState(ctx) {
     ctx.word = '';
     ctx.party = '';
     ctx.showParties = false;
